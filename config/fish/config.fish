9030c6d97134cfc61bf312683f3374a42b4e95bc
hermippus <x@x.x>
2025-01-02 17:16:06 +0500

    Update fish files

--- config/fish/config.fish
+++ config/fish/config.fish
@@ -1,2 +1,17 @@
+set fish_greeting
+
+## Aliases
+# LSD (LSDeluxe)
 alias ls='lsd'
-set fish_greeting ""
+
+# Pacman
+alias pacman-config="sudo nvim /etc/pacman.conf"
+alias arch-update="sudo pacman -Syyu"
+alias pacs="sudo pacman -S"
+alias pacr="sudo pacman -R"
+alias pacrns="sudo pacman -Rns"
+alias pacch="sudo pacman -Scc"
+
+
+
+
